dc3d85ec9a1aa56bf813539db081c4ef24c14fbc
NOONE <ranswnry@gmail.com>
2025-08-27 11:05:42 +0200

    Update Build.bat

--- File Manager/Build.bat
+++ File Manager/Build.bat
@@ -6,7 +6,7 @@ echo.
 echo N'utilise Build.bat uniquement pour compiler ton client.py (situé dans ./Client/client.py) une fois
 echo qu'il est bien configure.
 echo.
-echo Si tu sais pas comment utiliser, envoie un DM a @main.enc sur Discord.
+echo Si tu ne sais pas comment utiliser, envoie un DM a @main.enc sur Discord.
 echo.
 echo Si tu as deja configure, alors tu n'as qu'a appuyer sur une touche
 echo pour lancer la compilation du client vers un executable.
@@ -23,4 +23,5 @@ echo.
 echo.
 echo [+] Compilation terminée ! Le fichier compilé est situé dans ./Built/client.exe
 echo.
+
 pause
